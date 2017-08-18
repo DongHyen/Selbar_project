563983de53cfebe7f6162a0060f77fb87342e180
DongHyen <rlaehdgus83@naver.com>
2017-08-18 20:48:12 +0900

    Initial commit

---
+++ app/src/main/java/Fragment/Graph.java
@@ -0,0 +1,26 @@
+package Fragment;
+
+/**
+ * Created by DongHyen on 2017-08-18.
+ */
+
+/**
+ * Created by DongHyen on 2017-07-25.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Graph extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_graph, container, false);
+        return rootView;
+    }
+}
+
